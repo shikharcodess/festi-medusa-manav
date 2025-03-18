01cccb319d54e956a65be8e9844d4357c46c928f
Shikhar Yadav <yshikharfzd10@gmail.com>
2025-03-18 09:39:56 +0530

    service resolution error

--- medusa-config.ts
+++ medusa-config.ts
@@ -35,11 +35,11 @@ module.exports = defineConfig({
     //     ],
     //   },
     // },
+    // {
+    //   resolve: "./src/modules/sync-service",
+    // },
     {
-      resolve: "./src/modules/sync-service",
-    },
-    {
-      resolve: "./src/modules/vary",
+      resolve: "/final/.medusa/server/src/modules/vary",
       options: {
         varyApiUrl: "http://festi.vary.rent:1331/test/1",
         varyUser: "maastery-test",
